cd8026ad76341e106e4557bdaf7b4bf25f0a2556
ovkulkarni <2019okulkarn@tjhsst.edu>
2018-03-10 17:18:57 -0500

    add basic ui

--- hacktj/hacktj/urls.py
+++ hacktj/hacktj/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index_view, name='index'),
 ]
