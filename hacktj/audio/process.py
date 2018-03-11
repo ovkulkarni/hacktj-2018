cf5eebf24ed0eff89fb11f8b2821b97d3910662b
Charlie Gunn <cjgunnmail@gmail.com>
2018-03-11 08:54:37 -0400

    audio search recognizes phrases

--- hacktj/audio/process.py
+++ hacktj/audio/process.py
@@ -29,13 +29,28 @@ def combine_dicts(a, b):
 def word_data(video, split_length=55):
     fname = flac_from_video(video)
     fname_starts = split_by_seconds(fname, split_length)
-    data = {}
+    trans = []
+    order = {}
+    cur_word = 0
     for f, start in fname_starts:
-        data = search(f, data, add_time=start)
-    return data
+        order, trans, cur_word = search(f, order, trans, add_time=start, cur_word=cur_word)
+    return order, trans
 
+def find_matches(data, phrase):
+    order, trans = data
+    words = phrase.lower().split()
+    for word in words:
+        if word not in order: return None
+    if len(words) == 1:
+        return [t for t, p in order[words[0]]]
+    times = []
+    for time, pos in order[words[0]]:
+        for e, word in enumerate(words[1:], start=1):
+            if trans[pos+e] != word: break
+        else: times.append(time)
+    return sorted(times)
 
-def search(a, word_times, add_time=0):
+def search(a, word_order, trans, add_time=0, cur_word=0):
     r = sr.Recognizer()
     with sr.AudioFile(a) as source:
         audio = r.record(source)
@@ -47,10 +62,13 @@ def search(a, word_times, add_time=0):
             for inst in best_alt['words']:
                 time = float(inst['startTime'][:-1]) + add_time
                 word = inst['word'].lower()
-                if word in word_times:
-                    word_times[word].append(time)
-                else: word_times[word] = [time]
-    return word_times
+                if word in word_order:
+                    word_order[word].append((time, cur_word))
+                else:
+                    word_order[word] = [(time, cur_word)]
+                trans.append(word)
+                cur_word += 1
+    return word_order, trans, cur_word
 
 if __name__ == "__main__":
     x = word_data('trump.mp4')
